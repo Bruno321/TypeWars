bd490b36ad073f55630e45e79f3ed311f81e4b45
bruno <brunopaz00@yahoo.com.mx>
2020-12-15 19:35:08 -0600

    Added atributes to the profile and funcionality
    *new to the model: victorys, defeats
    *now when you win or lose is saved in the db

--- type_wars/game/consumers.py
+++ type_wars/game/consumers.py
@@ -2,7 +2,10 @@ import json
 from asgiref.sync import async_to_sync, sync_to_async
 
 # django
+from django.contrib.auth import get_user_model
 
+# local
+from users.models import Profile
 
 # channels
 from channels.generic.websocket import AsyncWebsocketConsumer
@@ -23,39 +26,27 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.channel_name
         )
 
-        # user = self.scope['user']
-        # if user.is_anonymous :
-        #     user = 'Guest'
-        # else:
-        #     user = user.username
-        
         await self.accept()
 
-        # await self.channel_layer.group_send(
-        #     self.room_group_name,
-        #     {
-        #         'type':'chat_message',
-        #         'message': {
-        #             'message': user
-                    
-        #         }
-        #     }
-        # )
-
-        # esto es un echo, solo el que lo ocasiona lo puede ver
-        # await self.send(text_data=json.dumps({
-        #     'usuario': user
-        # }))
-        # podemos usar await self.close() si se mete mas de uno
 
     async def disconnect(self,close_code):
+        
+        # 4000 is the victory close code
+        if close_code == 4000:
+            if not self.scope["user"].is_anonymous:
+                await database_sync_to_async(self.upload_victory_data)()
+        # 4001 is the defeat close code 
+        if close_code == 4001:
+            if not self.scope["user"].is_anonymous :
+                await database_sync_to_async(self.upload_defeat_data)()
+
+
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.room_name
         )
 
     async def receive(self,text_data):
-        print("recibido texto",text_data)
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
         
@@ -77,5 +68,23 @@ class ChatConsumer(AsyncWebsocketConsumer):
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
             'message':message,
-            # 'user':self.scope['user'].username
         }))
+
+    def upload_victory_data(self):
+        self.user_id = self.scope["user"].id
+        User = get_user_model()
+        
+        self.user = User.objects.get(id=self.user_id)
+
+        self.user.victorys = self.user.victorys + 1
+        self.user.save()
+
+    def upload_defeat_data(self):
+        self.user_id = self.scope["user"].id
+        User = get_user_model()
+        
+        self.user = User.objects.get(id=self.user_id)
+
+        self.user.defeats = self.user.defeats + 1
+        self.user.save()
+        
--- type_wars/users/models.py
+++ type_wars/users/models.py
@@ -4,3 +4,5 @@ from django.contrib.auth.models import User,AbstractUser
 # Create your models here.
 class Profile(AbstractUser):
     points = models.IntegerField(default=0)
+    victorys = models.IntegerField(default=0)
+    defeats = models.IntegerField(default=0)
